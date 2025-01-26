b197a1df2b41bdd32ebd93e9d2115ed97009a17e
Emanuele Disco <emawind84@gmail.com>
2025-01-26 14:03:19 +0900

    fix yaw angle desync happening when using normal teleport
    Whenever you use a normal (non-silent) teleport, you are locked in place for 18 tics (about half a second). If you turn your head during this time, it will cause a desync.
    
    (cherry picked from commit 780cf79354373f9aba8c9855a1048855622b80af)

--- src/gl/stereo3d/gl_openxrdevice.cpp
+++ src/gl/stereo3d/gl_openxrdevice.cpp
@@ -546,9 +546,10 @@ namespace s3d
         //Some crazy stuff to ascertain the actual yaw that doom is using at the right times!
         if (getGameState() != GS_LEVEL || getMenuState() != MENU_Off 
         || ConsoleState == c_down || ConsoleState == c_falling 
+        || (player && player->mo && player->mo->reactiontime > 0)
         || (player && player->playerstate == PST_DEAD)
         || (player && player->resetDoomYaw)
-        || paused 
+        || paused
         )
         {
             resetDoomYaw = true;
