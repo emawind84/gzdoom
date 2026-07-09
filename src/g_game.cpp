3fde1e30a4cd2d980c7b0eead35435862f8ae613
Emanuele Disco <emawind84@gmail.com>
2026-07-09 23:53:33 +0900

    fix: fix horizontal camera freeze during death state
    Move resetDoomYaw from PST_DEAD loop to PlayerReborn
    Prevents the horizontal camera from being locked while dead while still ensuring proper orientation synchronization upon player respawn.

--- src/g_game.cpp
+++ src/g_game.cpp
@@ -1527,6 +1527,7 @@ void FLevelLocals::PlayerReborn (int player)
 	p->ohattackdown = true;
 	p->original_oldbuttons = ~0;
 	p->playerstate = PST_LIVE;
+	p->resetDoomYaw = true;
 	NetworkEntityManager::SetClientNetworkEntity(p);
 
 	if (gamestate != GS_TITLELEVEL)
--- src/rendering/gl/stereo3d/gl_openvr.cpp
+++ src/rendering/gl/stereo3d/gl_openvr.cpp
@@ -2447,8 +2447,7 @@ namespace s3d
 				resetDoomYaw = false;
 			}
 			else if (gamestate != GS_LEVEL || menuactive != MENU_Off 
-			|| ConsoleState == c_down || ConsoleState == c_falling 
-			|| (player && player->playerstate == PST_DEAD)
+			|| ConsoleState == c_down || ConsoleState == c_falling
 			|| (player && player->resetDoomYaw)
 			|| paused 
 			)
